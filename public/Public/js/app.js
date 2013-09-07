052ebc9c1780181b06312c4d62c8e51ae5905742
chenq1985626 <371043483@qq.com>
2013-09-07 14:41:48 +0800

    登录，分类，监听分页
    登录，分类，监听分页

--- public/Public/js/app.js
+++ public/Public/js/app.js
@@ -1,49 +1,63 @@
 $(function(){
+     window.is_login=false;
     //接受用户登陆信息
+    $(window).on('message.userinfo',function(e){
+        var data=e.originalEvent.data;
+        if('object'==$.type(data) && data.token){
+            get_user_info(data.token,function(d){
+             window.U=d; //存储用户信息
+             window.is_login=true;
+             var userinfo_html=TPM.parseTpl($('#userinfo_tpl').html(),{avatar:U.profileImageUrl,username:U.screenName});
+             $('#userinfo').html(userinfo_html);
+            });         
+        }
+    });
+ 
+    
     //显示分类
-    //监听分页
-    //监听滑动
+    var cat_html=TPM.parseTpl($('#cat_tpl').html(),{cats:cats});
+    $('#cat').html(cat_html);
+
+     //分析hash, 获得文章分类和文章ID  
+     var cat_id,article_id;
+     if(location.hash.indexOf('_')){
+        var hash=location.hash.substr(1).split('_');
+        cat_id=hash[0];
+        article_id=hash[1];
+     }
+     var cat_id=cat_id || default_cat_id;
+     show_article_list(cat_id);
+
+     if(article_id){
+         show_article(article_id); 
+         //如果是手机访问，则隐藏侧栏
+         if(Modernizr.mq('(max-width:600px)')){
+            $('#menubar,#sidebar').addClass('movetoleft')
+         } 
+     }
+
+     //监听分页
+     $('#article_more').on('click',function(){
+        var cat_id=$(this).data('cat_id');
+        var page=$(this).data('page');
+        show_article_list(cat_id,page);
+     });
+     //监听滑动
+      $('#menubar,#sidebar').on('swipeLeft',function(){
+         if(Modernizr.mq("(max-width:600px)")){
+           $(this).addClass('movetoleft');
+          }
+      }); 
+      $('#sidebar').on('swipeRight',function(){
+         if(Modernizr.mq("(max-width:600px)")){
+            $('#menubar').removeClass('movetoleft'); 
+           }
+      });
+      $('#content').on('swipeRight',function(){
+         if(Modernizr.mq("(max-width:600px)")){
+            $('#sidebar').removeClass('movetoleft');    
+          }
+      });
+
+  
 });
-
-
-//绑定账户
-function login(platform){
-}
-
-
-//发布文章的弹出框
-
-function publish(){
-}
-
-function add_article(){
-}
-
-//编辑文章
-function edit(){
-
-}
-
-//获得用户信息
-
-function get_user_info(token,cb){
-}
-
-
-
-//显示文章列表
-function show_article_list(cat_id,page){
-}
-
-function show_article_list_and_swipe(cat_id,page){
-}
-
-
-//显示某一篇文章
-function show_article(id){
-}
-
-
-//删除文章
-function delete_article(id){
-}
