ea71832658107522e907d85fbb4c3b7e46337738
luofei614 <upfy@qq.com>
2013-09-07 09:18:03 +0800

    add TPM

---
+++ public/Public/js/config.js
@@ -0,0 +1,11 @@
+AV.initialize("fan7es8i7rat0a3rcxo18fxl6mp6a2eq4mffdpcyeuo5uoju", "9n9vm9kvj6eeet2qxgnvh6kq5zrjp520kw9uxwlygu8qj1x7");
+//分类
+var cats={
+    'programmer':'程序员',
+    'designer':'设计师',
+    'fun':'娱乐',
+    'jnsx':'江南水乡'
+}
+
+var default_cat_id='programmer';// 默认选择的分类
+
