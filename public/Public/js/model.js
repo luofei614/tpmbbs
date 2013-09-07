a56f521bfe6783d8bef37b13cabc44d4f896b997
chenq1985626 <371043483@qq.com>
2013-09-07 09:56:22 +0800

    更新文章
    更新文章

--- public/Public/js/model.js
+++ public/Public/js/model.js
@@ -12,4 +12,17 @@ var A=AV.Object.extend('article',{
             avatar:avatar
         }); 
     },
+    //更新文章
+    update:function(id,title,content,cat_id,uid,username,avatar){
+        var article=new A();
+        article.id=id;
+        return article.save({
+            title:title,
+            content:content,
+            cat_id:cat_id,
+            uid:uid,
+            username:username,
+            avatar:avatar
+        }); 
+    },
 });
