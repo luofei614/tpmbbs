a8a119b3b5fa08b389e3c37268c9bd55681aaf66
luofei614 <upfy@qq.com>
2013-09-07 10:25:18 +0800

    app.js中的函数架构

---
+++ public/Public/js/app.js
@@ -0,0 +1,49 @@
+$(function(){
+    //接受用户登陆信息
+    //显示分类
+    //监听分页
+    //监听滑动
+});
+
+
+//绑定账户
+function login(platform){
+}
+
+
+//发布文章的弹出框
+
+function publish(){
+}
+
+function add_article(){
+}
+
+//编辑文章
+function edit(){
+
+}
+
+//获得用户信息
+
+function get_user_info(token,cb){
+}
+
+
+
+//显示文章列表
+function show_article_list(cat_id,page){
+}
+
+function show_article_list_and_swipe(cat_id,page){
+}
+
+
+//显示某一篇文章
+function show_article(id){
+}
+
+
+//删除文章
+function delete_article(id){
+}
